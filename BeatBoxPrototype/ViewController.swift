84c3baf0350ace9037b5af5413c60c0f14167888
Jakob Wilter <jakobwilter@gmail.com>
2019-04-18 15:51:11 -0700

    Initial Commit

---
+++ BeatBoxPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeatBoxPrototype
+//
+//  Created by Jakob Wilter on 4/18/19.
+//  Copyright © 2019 Jakob Wilter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
